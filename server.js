c2b4c2c0876b5397db54538e8f8f2332a3729d39
classproject01 <projectclass65@gmail.com>
2025-06-26 22:14:49 +0100

    setting up mongoose

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = require("./config/db");
 const app = express();
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 app.listen(PORT, ()=>console.log(`server is running now on ${PORT}`));
